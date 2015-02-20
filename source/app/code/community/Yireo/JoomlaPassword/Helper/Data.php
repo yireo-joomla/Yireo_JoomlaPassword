181d121219b7eef5df3e1e3a47932191bb1de491
Jisse Reitsma <info@yireo.com>
2015-02-20 16:14:50 +0100

    Version increment to indicate new Joomla hashing support

--- source/app/code/community/Yireo/JoomlaPassword/Helper/Data.php
+++ source/app/code/community/Yireo/JoomlaPassword/Helper/Data.php
@@ -3,7 +3,7 @@
  * Yireo JoomlaPassword for Magento 
  *
  * @author Yireo
- * @copyright Copyright 2014
+ * @copyright Copyright 2015
  * @license Open Source License
  * @link http://www.yireo.com
  */
--- source/app/code/community/Yireo/JoomlaPassword/Model/Encryption.php
+++ source/app/code/community/Yireo/JoomlaPassword/Model/Encryption.php
@@ -3,7 +3,7 @@
  * Yireo JoomlaPassword for Magento 
  *
  * @author Yireo
- * @copyright Copyright 2014
+ * @copyright Copyright 2015
  * @license Open Source License
  * @link http://www.yireo.com
  */
@@ -13,18 +13,57 @@
  */
 class Yireo_JoomlaPassword_Model_Encryption extends Mage_Core_Model_Encryption
 {
+    /**
+     * Validate hash against hashing method (with or without salt)
+     *
+     * @param string $password
+     * @param string $hash
+     * @return bool
+     * @throws Exception
+     */
     public function validateHash($password, $hash)
     {
+        // Explode the hash
         $hashArr = explode(':', $hash);
+
+        // Joomla PHPass hashes
+		if (strpos($hash, '$P$') === 0) {
+            include_once BP.'lib/JoomlaPassword/PasswordHash.php';
+            if(class_exists('PasswordHash')) {
+                $phpass = new PasswordHash(10, true);
+	    		return $phpass->CheckPassword($password, $hash);
+            }
+
+        // Native PHP password hashing
+		} elseif ($hash[0] == '$' && function_exists('password_verify')) {
+            return password_verify($password, $hash);
+
+        // SHA256 (unsupported)
+        } elseif (substr($hash, 0, 8) == '{SHA256}') {
+            return false; 
+        }
+
+        // Regular hashing
         switch (count($hashArr)) {
             case 1:
-                return $this->hash($password) === $hash;
+                // Original MD5 hashes
+                if(strlen($hash) == 32) {
+                    return $this->hash($password) === $hash;
+                }
+
             case 2:
+
+                // Original Magento hashing
                 $rt = $this->hash($hashArr[1] . $password) === $hashArr[0];
+
+                // Joomla hashing
                 if($rt == false) $rt = $this->hash($password.$hashArr[1]) === $hashArr[0];
+
+                // Joomla reverse hashing
                 if($rt == false) $rt = $this->hash($password.$hashArr[0]) === $hashArr[1];
                 return $rt;
         }
+
         Mage::throwException('Invalid hash.');
     }
 
